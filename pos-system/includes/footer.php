28692d6712a7864a6b01d018358363f6950d78d6
Jomari Longboy <Jomarilongboy7@gmail.com>
2026-08-03 09:04:33 +0800

    Delete pos-system directory

--- pos-system/includes/footer.php
+++
@@ -1,7 +0,0 @@
-        </div> <!-- End row -->
-    </div> <!-- End container-fluid -->
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/script.js"></script>
-</body>
-</html>
