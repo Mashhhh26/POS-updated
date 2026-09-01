f0d0f12c127b3c968224045137c780f3e44b0241
Jomari Longboy <Jomarilongboy7@gmail.com>
2026-09-01 10:08:56 +0800

    Delete pos-system directory

--- pos-system/actions/clear-cart.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../includes/functions.php';
-
-$_SESSION['cart'] = [];
-redirect('../pages/sales.php');
-?>
--- pos-system/includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'functions.php';
-
-if (!isLoggedIn()) {
-    redirect('../pages/login.php');
-    exit();
-}
-?>
